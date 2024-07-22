c8291829683da3eff3ba1af1ad401c4ca9fe886e
Harsh <auharshitsingh@gmail.com>
2024-07-22 23:06:57 +1000

    authentication and navigation done

--- amplify-react-app/src/App.js
+++ amplify-react-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import Home from "./Home";
+import Portal from "./Portal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/portal">Portal</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/portal" component={Portal} />
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
