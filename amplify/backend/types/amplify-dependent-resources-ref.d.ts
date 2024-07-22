f90af95ef7a00ad179b0c25a5b5c76752923076b
Harsh <auharshitsingh@gmail.com>
2024-07-22 17:04:22 +1000

    fetching and displaying files from S3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "files": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
